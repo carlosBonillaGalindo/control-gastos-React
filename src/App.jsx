2026ce11e252b53c77708f578890e53741deaaab
Carlos Bonilla <bonillagalindocarlos@gmail.com>
2023-06-09 00:07:47 -0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>Control de gastos</h1>
+  )
+}
+
+export default App
