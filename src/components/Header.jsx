3c772b929d2b63b81a153dea09b1fe6f03c6629c
Carlos Bonilla <bonillagalindocarlos@gmail.com>
2023-08-31 14:04:00 -0600

    Nuevo Presupuesto Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import NuevoPresupuesto from "./NuevoPresupuesto";
 
 const Header = () => { 
     return (
         <header>
             <h1>Planificador de Gastos</h1>
+            <NuevoPresupuesto></NuevoPresupuesto>
         </header>
     )
 }
---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NuevoPresupuesto = () => {
+    return(
+        <div className="contenedor-presupuesto contenedor sombra">
+            <form className="formulario">
+                <div className="campo">
+                    <label>Definir Presupuesto</label>
+                    <input 
+                        type="text" 
+                        className="nuevo-presupuesto"
+                        placeholder="Añde tu Presupuesto"
+                    />
+                </div>
+                <input type="submit" value="Añadir" />
+            </form>
+
+        </div>
+    )
+}
+
+export default NuevoPresupuesto
